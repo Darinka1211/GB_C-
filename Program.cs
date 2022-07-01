f8b70c0385b38ca16509e04be9a174cf4f4db0ca
Darinka1211 <93863016+Darinka1211@users.noreply.github.com>
2022-07-02 01:23:19 +0300

    task_2

--- Program.cs
+++ Program.cs
@@ -4,22 +4,50 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
+// Console.Write("Введите первое число:");
+// int numberOne = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число:");
+// int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+// if (numberOne>numberTwo)
+// {
+// Console.Write("Максимальное число:");
+// Console.WriteLine(numberOne);
+// Console.Write("Минимальное число:");
+// Console.WriteLine(numberTwo);
+// }
+// else
+// {
+// Console.Write("Максимальное число:");
+// Console.WriteLine(numberTwo);
+// Console.Write("Минимальное число:");
+// Console.WriteLine(numberOne);
+// }
+//-------------------------------------------
+//Задача 2
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
 Console.Write("Введите первое число:");
 int numberOne = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число:");
 int numberTwo = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число:");
+int numberThree = Convert.ToInt32(Console.ReadLine());
 
-if (numberOne>numberTwo)
+if (numberOne > numberTwo && numberOne > numberThree)
 {
-Console.Write("Максимальное число:");
-Console.WriteLine(numberOne);
-Console.Write("Минимальное число:");
-Console.WriteLine(numberTwo);
+    Console.Write("Максимальное число:");
+    Console.WriteLine(numberOne);
 }
-else
+if (numberTwo > numberOne && numberTwo > numberThree)
 {
-Console.Write("Максимальное число:");
-Console.WriteLine(numberTwo);
-Console.Write("Минимальное число:");
-Console.WriteLine(numberOne);
+    Console.Write("Максимальное число:");
+    Console.WriteLine(numberTwo);
+}
+if (numberThree > numberTwo && numberThree > numberOne)
+{
+    Console.Write("Максимальное число:");
+    Console.WriteLine(numberThree);
 }
